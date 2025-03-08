adc09a3cd6570afd4ccc24d02e5dcf5717c9c8d4
melrwa <orwamelkizedek@gmail.com>
2025-03-08 14:56:29 +0300

    added run.py for deployment

--- app/run.py
+++ app/run.py
@@ -1,6 +1,14 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Get the port from the environment variable (required for Render)
+    port = int(os.getenv("PORT", 5000))
+    @app.route('/')
+    def hello():
+        return "This Server is running"
+    
+    # Run the app
+    app.run(host="0.0.0.0", port=port, debug=app.config["DEBUG"])
