4bf541abc4f701dcd0b9c0b057dc7b0077a181cd
melrwa <orwamelkizedek@gmail.com>
2025-03-09 04:02:00 +0300

    updated the bookiing schema

--- app/schemas/bookings_schema.py
+++ app/schemas/bookings_schema.py
@@ -1,3 +1,5 @@
+from app.schemas.bus_schema import BusSchema
+from app.schemas.user_schema import UserSchema
 from marshmallow import Schema, fields, validate
 
 class BookingSchema(Schema):
@@ -7,6 +9,8 @@ class BookingSchema(Schema):
     seat_number = fields.Int(required=True, validate=validate.Range(min=1))
     booking_date = fields.DateTime(required=True)
     status = fields.Str(validate=validate.OneOf(['confirmed','pending', 'cancelled']))
+    customer = fields.Nested(UserSchema)  # Include if needed
+    bus = fields.Nested(BusSchema)        # Include if needed
 
     class Meta:
-        fields = ('id', 'customer_id', 'bus_id', 'seat_number', 'booking_date', 'status')
+        fields = ('id', 'customer_id', 'bus_id', 'seat_number', 'booking_date', 'status', 'customer', 'bus')
